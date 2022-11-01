befb72cd5d62088efb1e531db00a2486625ffa8f
Tayyam <115746736+Tayyam@users.noreply.github.com>
2022-11-02 00:21:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function btnhide(){
+        document.getElementById("sectionmain2").style.display = "none";
+        document.getElementById("btnshow").style.display = "flex";
+ }
+ function btnshow(){
+    document.getElementById("sectionmain2").style.display = "flex";
+    document.getElementById("btnshow").style.display = "none";
+}
